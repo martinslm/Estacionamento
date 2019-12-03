be07f455be1ecf5ae58c460f093292a82db3cc64
Larissa Martins Mauricio <larissa.jucre@gmail.com>
2019-12-02 21:42:01 -0300

    Parte final
    Parte final

--- EditorForms/Estacionamento.Designer.cs
+++ EditorForms/Estacionamento.Designer.cs
@@ -34,8 +34,11 @@
             this.lbErroEntrada = new System.Windows.Forms.Label();
             this.lbVagasOcupadas = new System.Windows.Forms.Label();
             this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbSaida = new System.Windows.Forms.TextBox();
             this.btnSaida = new MaterialSkin.Controls.MaterialFlatButton();
+            this.lbRetornoSaida = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.lbVagasDisponiveis = new MaterialSkin.Controls.MaterialLabel();
             this.SuspendLayout();
             // 
             // materialLabel1
@@ -105,19 +108,19 @@
             this.materialLabel2.TabIndex = 6;
             this.materialLabel2.Text = "Informe as Vagas que deseja liberar. \n Ex. 299,1,3,5";
             // 
-            // textBox1
+            // tbSaida
             // 
-            this.textBox1.Location = new System.Drawing.Point(378, 340);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(151, 20);
-            this.textBox1.TabIndex = 7;
+            this.tbSaida.Location = new System.Drawing.Point(378, 340);
+            this.tbSaida.Name = "tbSaida";
+            this.tbSaida.Size = new System.Drawing.Size(151, 20);
+            this.tbSaida.TabIndex = 7;
             // 
             // btnSaida
             // 
             this.btnSaida.AutoSize = true;
             this.btnSaida.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnSaida.Depth = 0;
-            this.btnSaida.Location = new System.Drawing.Point(559, 331);
+            this.btnSaida.Location = new System.Drawing.Point(567, 331);
             this.btnSaida.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnSaida.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnSaida.Name = "btnSaida";
@@ -128,13 +131,53 @@
             this.btnSaida.UseVisualStyleBackColor = true;
             this.btnSaida.Click += new System.EventHandler(this.btnSaida_Click);
             // 
+            // lbRetornoSaida
+            // 
+            this.lbRetornoSaida.AutoSize = true;
+            this.lbRetornoSaida.Depth = 0;
+            this.lbRetornoSaida.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lbRetornoSaida.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbRetornoSaida.Location = new System.Drawing.Point(201, 403);
+            this.lbRetornoSaida.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lbRetornoSaida.Name = "lbRetornoSaida";
+            this.lbRetornoSaida.Size = new System.Drawing.Size(0, 19);
+            this.lbRetornoSaida.TabIndex = 9;
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(665, 208);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(133, 19);
+            this.materialLabel3.TabIndex = 10;
+            this.materialLabel3.Text = "Vagas Disponiveis";
+            // 
+            // lbVagasDisponiveis
+            // 
+            this.lbVagasDisponiveis.AutoSize = true;
+            this.lbVagasDisponiveis.Depth = 0;
+            this.lbVagasDisponiveis.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lbVagasDisponiveis.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbVagasDisponiveis.Location = new System.Drawing.Point(680, 237);
+            this.lbVagasDisponiveis.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lbVagasDisponiveis.Name = "lbVagasDisponiveis";
+            this.lbVagasDisponiveis.Size = new System.Drawing.Size(0, 19);
+            this.lbVagasDisponiveis.TabIndex = 11;
+            // 
             // Estacionamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 550);
+            this.Controls.Add(this.lbVagasDisponiveis);
+            this.Controls.Add(this.materialLabel3);
+            this.Controls.Add(this.lbRetornoSaida);
             this.Controls.Add(this.btnSaida);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbSaida);
             this.Controls.Add(this.materialLabel2);
             this.Controls.Add(this.lbVagasOcupadas);
             this.Controls.Add(this.lbErroEntrada);
@@ -156,7 +199,10 @@
         private System.Windows.Forms.Label lbErroEntrada;
         private System.Windows.Forms.Label lbVagasOcupadas;
         private MaterialSkin.Controls.MaterialLabel materialLabel2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbSaida;
         private MaterialSkin.Controls.MaterialFlatButton btnSaida;
+        private MaterialSkin.Controls.MaterialLabel lbRetornoSaida;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private MaterialSkin.Controls.MaterialLabel lbVagasDisponiveis;
     }
 }
--- EditorForms/Estacionamento.cs
+++ EditorForms/Estacionamento.cs
@@ -10,17 +10,21 @@ namespace EditorForms
         Semaphore pool = new Semaphore(1, 1);
         string mensagem;
         string vagasOcupadas = string.Empty;
+        string resultVagasLiberadas = string.Empty;
+        private IList<int> vagasLiberar = new List<int>();
         private IList<Vaga> vagas = new List<Vaga>();
+        int vagasDisponiveis = 500;
 
         public Estacionamento()
         {
             InitializeComponent();
             InicializarVagas();
+            AtualizarLabelVagas();
         }
 
         private void InicializarVagas()
         {
-            for (int i = 1; i < 500; i++)
+            for (int i = 1; i <= 500; i++)
             {
                 vagas.Add(new Vaga { NumeroVaga = i, Ocupada = false });
             }
@@ -48,6 +52,7 @@ namespace EditorForms
             }
 
             lbVagasOcupadas.Text = string.Format("Vagas Ocupadas: {0}", vagasOcupadas.Remove(vagasOcupadas.Length - 1));
+            AtualizarLabelVagas();
             vagasOcupadas = string.Empty;
         }
 
@@ -56,11 +61,16 @@ namespace EditorForms
             pool.WaitOne();
 
             if (!vagas.Where(v => v.Ocupada == false).Any())
+            {
                 mensagem = "ESTACIONAMENTO LOTADO.";
+                pool.Release();
+                return;
+            }
 
             int vagaParaOcupar = vagas.Where(v => v.Ocupada == false).Select(v => v.NumeroVaga).First();
 
             vagas[vagaParaOcupar - 1].Ocupada = true;
+            vagasDisponiveis--;
 
             vagasOcupadas += string.Format("{0},", vagaParaOcupar);
 
@@ -69,7 +79,56 @@ namespace EditorForms
 
         private void btnSaida_Click(object sender, EventArgs e)
         {
+            string[] vagasSaida = tbSaida.Text.Split(',');
 
+            foreach (var vetor in vagasSaida)
+            {
+                vagasLiberar.Add(Convert.ToInt32(vetor));
+            }
+
+            for(int i = vagasSaida.Count(); i>0; i--)
+            {
+                Thread t1 = new Thread(new ThreadStart(DesocuparVagasEstacionamento));
+                t1.Start();
+                t1.Join();
+            }
+
+            lbRetornoSaida.Text = resultVagasLiberadas;
+            AtualizarLabelVagas();
+            resultVagasLiberadas = string.Empty;
+        }
+
+        private void DesocuparVagasEstacionamento()
+        {
+            pool.WaitOne();
+            int vaga = vagasLiberar.First();
+
+            try
+            {
+                if (vagas.Where(v => v.NumeroVaga == vaga).FirstOrDefault().Ocupada == false)
+                {
+                    resultVagasLiberadas += string.Format("A vaga {0} já encontrava-se disponível. Por favor verifique o valor informado. \n", vaga);
+                }
+                else
+                {
+                    vagas[vaga - 1].Ocupada = false;
+                    vagasDisponiveis++;
+                    resultVagasLiberadas += string.Format("A vaga {0} foi liberada com sucesso. \n", vaga);
+                }
+            }
+            catch
+            {
+                resultVagasLiberadas += string.Format("A vaga {0} não foi encontrada. \n", vaga);
+            }
+
+            vagasLiberar.Remove(vaga);
+
+            pool.Release();
+        }
+
+        private void AtualizarLabelVagas()
+        {
+            lbVagasDisponiveis.Text = vagasDisponiveis.ToString();
         }
     }
 }
